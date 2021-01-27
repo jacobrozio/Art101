4c12005b18acc10f56b3b1ac1a13ad166e594b13
jacobrozio <jrozio@ucsc.edu>
2021-01-27 11:21:32 -0800

    lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,7 @@
+//Load 1 of 2 form types.
+    //If loading grid form load the 3x3 image grid.
+    //If text field form text form load images and text field.
+//Accept user input.
+//Evaluate user input for accuracy.
+    //If user input is accurate, load the next page.
+    //If user input is not accurate, generate new form and display.
---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ * Author:    Jacob Rozio
+ * Created:   1.27.2021
+ * Additional notes: N/A
+ *
+ **/
+
+ //variable definitions and declarations
+ var make = "Volkswagen";
+ var model = "Jetta";
+ var color = "white";
+ var year = 2018;
+
+
+ var age = 2021 - year
+
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + "<br>");
