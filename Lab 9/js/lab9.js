7165348b25448fc563590ff8cbe014604b228c73
jacobrozio <jrozio@ucsc.edu>
2021-02-10 13:52:47 -0800

    Lab 9

---
+++ Lab 9/js/lab9.js
@@ -0,0 +1,25 @@
+/**
+ * Author:   Jacob Rozio
+ * Created:   2.10.2021
+ *
+ * (c) Copyright by NA
+ **/
+
+
+ var outputEl = document.getElementById("output")
+ var new1El = document.createElement("p")
+
+ new1El.innerHTML = "Hello Everyone!"
+ var new2El = document.createElement("o")
+
+ new2El.innerHTML = "My name is Jacob Rozio"
+
+ output.appendChild(new1El)
+ output.appendChild(new2El)
+
+output.style.backgroundColor= "green";
+output.style.marginRight= "400px";
+output.style.fontFamily= "Courier New"
+
+output2.style.backgroundColor= "white";
+output2.style.fontFamily= "Times New Roman";
